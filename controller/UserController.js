f388ffe3360c552737ff76d500ebdf4cf393f8ba
ozen-hub <info.ozenhub@gmail.com>
2022-11-13 19:40:10 +0530

    2

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,16 +1,28 @@
 const UserSchema = require('../model/User');
-const signup=(req,resp)=>{
-    const user= new UserSchema({
+const signup = (req, resp) => {
+    const user = new UserSchema({
         email: req.body.email,
         password: req.body.password,
         fullName: req.body.fullName
     });
-    user.save().then(result=>{
-        resp.json({data:{status:201,message:"Registered"}});
-    }).catch(error=>{
+
+    UserSchema.findOne({email: req.body.email}).then(existsData => {
+        if (existsData === null) {
+            user.save().then(result => {
+                resp.json({data: {status: 201, message: "Registered", result}});
+            }).catch(error => {
+                console.log(error);
+                resp.json(error);
+            })
+        } else {
+            resp.status(403).json({data: {status: 403, message: "Already exists"}});
+        }
+    }).catch(error => {
         console.log(error);
         resp.json(error);
     })
+
+
 };
 module.exports = {
     signup
