269305e1676f06ed1c8dc0e2571e7bbd7df828ac
ozen-hub <info.ozenhub@gmail.com>
2022-11-13 19:49:23 +0530

    3

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,19 +1,29 @@
 const UserSchema = require('../model/User');
+const bcrypt = require('bcrypt');
+
 const signup = (req, resp) => {
-    const user = new UserSchema({
-        email: req.body.email,
-        password: req.body.password,
-        fullName: req.body.fullName
-    });
+
 
     UserSchema.findOne({email: req.body.email}).then(existsData => {
         if (existsData === null) {
-            user.save().then(result => {
-                resp.json({data: {status: 201, message: "Registered", result}});
-            }).catch(error => {
-                console.log(error);
-                resp.json(error);
-            })
+
+            bcrypt.hash(req.body.password, 10, function(err, hash) {
+
+                const user = new UserSchema({
+                    email: req.body.email,
+                    password: hash,
+                    fullName: req.body.fullName
+                });
+
+                user.save().then(result => {
+                    resp.json({data: {status: 201, message: "Registered", result}});
+                }).catch(error => {
+                    console.log(error);
+                    resp.json(error);
+                })
+
+            });
+
         } else {
             resp.status(403).json({data: {status: 403, message: "Already exists"}});
         }
